27a7794a13eb1f66b14b2247e166ed1487ebc19d
guadalupe.santamarina <guasantamarina@gmail.com>
2021-11-24 15:22:28 -0300

    Este es mi primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola, esta es mi app de prueba        
         </p>
         <a
           className="App-link"
